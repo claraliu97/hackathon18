114cb83bea5cbd455ab0a1ee86bcd8f2a5e03358
Clara Liu <claraliu97@gmail.com>
2018-03-04 03:33:20 -0500

    worked -history

--- manualscript.js
+++ manualscript.js
@@ -164,12 +164,241 @@ function popAlert(prescription,targetTime,myInterval) {
       timepoints.shift()
       targetTime = new Date(timepoints[0])
       prescription.sound.pause()
+      updatechart(1)
       break;
   }
   console.log('hahahah: '+value)
 });
 }
 
+function addToCalendar() {
+        document.getElementById("drugs1").innerHTML="Drug Name: Amoxicillin";
+        document.getElementById("drugs2").innerHTML="Dosage: 775 mg";
+        document.getElementById("drugs3").innerHTML="Times per Day: 3";
+        document.getElementById("drugs4").innerHTML="Length: 10 days";
+      }
+
 $( document ).ready(function() {
     select("visualization");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function updatechart(value){
+  document.getElementById("demo").innerHTML = value;
+  AmCharts.makeChart("chartdiv",{
+     "type": "serial",
+     "categoryField": "category",
+     "angle": 30,
+     "depth3D": 30,
+     "startDuration": 1,
+     "categoryAxis": {
+     "gridPosition": "start",
+     "fontSize": 5,
+     "labelOffset": 1,
+     "titleBold": false
+     },
+     "trendLines": [],
+     "graphs": [
+                {
+                "balloonText": "[[title]] on [[category]]:[[value]]",
+                "bulletBorderThickness": 0,
+                "fillAlphas": 1,
+                "fillColors": "#00D33F",
+                "id": "AmGraph-1",
+                "lineColor": "#00D33F",
+                "title": "Number Taken On Time",
+                "type": "column",
+                "valueField": "column-1"
+                },
+                {
+                "balloonText": "[[title]] on [[category]]:[[value]]",
+                "fillAlphas": 1,
+                "fillColors": "#F40E0E",
+                "id": "AmGraph-2",
+                "lineColor": "#FF0000",
+                "title": "Number Missed",
+                "type": "column",
+                "valueField": "column-2"
+                }
+                ],
+     "guides": [],
+     "valueAxes": [
+                   {
+                   "id": "ValueAxis-1",
+                   "stackType": "regular",
+                   "labelFrequency": 2,
+                   "title": ""
+                   },
+                   {
+                   "id": "ValueAxis-2"
+                   }
+                   ],
+     "allLabels": [],
+     "balloon": {},
+     "titles": [
+                {
+                "id": "title",
+                "size": 15,
+                "text": "Past 7 Days"
+                }
+                ],
+     "dataProvider": [
+                      {
+                      "category": "01-10",
+                      "column-1": "1",
+                      "column-2": "2"
+                      },
+                      {
+                      "category": "01-11",
+                      "column-1": "2",
+                      "column-2": "1"
+                      },
+                      {
+                      "category": "01-12",
+                      "column-1": "3",
+                      "column-2": "0"
+                      },
+                      {
+                      "category": "01-13",
+                      "column-1": "2",
+                      "column-2": "1"
+                      },
+                      {
+                      "category": "01-14",
+                      "column-1": "3",
+                      "column-2": "0"
+                      },
+                      {
+                      "category": "01-15",
+                      "column-1": "1",
+                      "column-2": "2"
+                      },
+                      {
+                      "category": "01-16",
+                      "column-1": value,
+                      "column-2": "0"
+                      }
+                      ]
+     }
+     );
+
+}
+AmCharts.makeChart("chartdiv",{
+   "type": "serial",
+   "categoryField": "category",
+   "angle": 30,
+   "depth3D": 30,
+   "startDuration": 1,
+   "categoryAxis": {
+   "gridPosition": "start",
+   "fontSize": 5,
+   "labelOffset": 1,
+   "titleBold": false
+   },
+   "trendLines": [],
+   "graphs": [
+              {
+              "balloonText": "[[title]] on [[category]]:[[value]]",
+              "bulletBorderThickness": 0,
+              "fillAlphas": 1,
+              "fillColors": "#00D33F",
+              "id": "AmGraph-1",
+              "lineColor": "#00D33F",
+              "title": "Number Taken On Time",
+              "type": "column",
+              "valueField": "column-1"
+              },
+              {
+              "balloonText": "[[title]] on [[category]]:[[value]]",
+              "fillAlphas": 1,
+              "fillColors": "#F40E0E",
+              "id": "AmGraph-2",
+              "lineColor": "#FF0000",
+              "title": "Number Missed",
+              "type": "column",
+              "valueField": "column-2"
+              }
+              ],
+   "guides": [],
+   "valueAxes": [
+                 {
+                 "id": "ValueAxis-1",
+                 "stackType": "regular",
+                 "labelFrequency": 2,
+                 "title": ""
+                 },
+                 {
+                 "id": "ValueAxis-2"
+                 }
+                 ],
+   "allLabels": [],
+   "balloon": {},
+   "titles": [
+              {
+              "id": "title",
+              "size": 15,
+              "text": "Past 7 Days"
+              }
+              ],
+   "dataProvider": [
+                    {
+                    "category": "01-10",
+                    "column-1": "1",
+                    "column-2": "2"
+                    },
+                    {
+                    "category": "01-11",
+                    "column-1": "2",
+                    "column-2": "1"
+                    },
+                    {
+                    "category": "01-12",
+                    "column-1": "3",
+                    "column-2": "0"
+                    },
+                    {
+                    "category": "01-13",
+                    "column-1": "2",
+                    "column-2": "1"
+                    },
+                    {
+                    "category": "01-14",
+                    "column-1": "3",
+                    "column-2": "0"
+                    },
+                    {
+                    "category": "01-15",
+                    "column-1": "1",
+                    "column-2": "2"
+                    },
+                    {
+                    "category": "01-16",
+                    "column-1": "0",
+                    "column-2": "0"
+                    }
+                    ]
+   }
+   );
+   */
